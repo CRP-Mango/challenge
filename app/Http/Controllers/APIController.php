ce879ccc5041790145e41482a1d6998787346eba
JoaoJr <joao.junior7cc@gmail.com>
2021-05-28 10:05:04 -0300

    API locale finalizado

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -9,18 +9,48 @@ class APIController extends Controller
     public function getlocale(Request $request)
     {
         $locales_json = json_decode( file_get_contents( 'base/locales.json' ) );
-        $locale_id = $request->input('id');
+        $locales_params = $request->all();
+        $response       = [];
 
-        #dd($locale_id);
-        if( !$locale_id ) {
+        if( !count($locales_params)  ) {//returne tudo
             return response()->json($locales_json, 200);
         }
 
         foreach ($locales_json as $locale) {
-            if($locale->id == $locale_id){
+            if( isset($locales_params['id']) && $locale->id == $locales_params['id'] ){
                 return response()->json($locale);
             }
+            if( isset($locales_params['name']) &&
+                strpos(strtolower($locale->name), strtolower($locales_params['name'])) !== false ){//achou params
+                if ( !array_key_exists($locale->id,$response ) ) {//se ja foi add ao response
+                    $response = $response+[$locale->id => $locale];//merge
+                }
+            }
+            if( isset($locales_params['state']) &&
+                strpos(strtolower($locale->state),strtolower($locales_params['state'])) !==  false  ){
+                if ( !array_key_exists($locale->id,$response) ) {
+                    $response = $response+[$locale->id => $locale];
+                }
+            }
+            if( isset($locales_params['latitude']) &&
+                strpos(strtolower(''.$locale->latitude),strtolower(''.$locales_params['latitude'])) !== false  ){
+                if ( !array_key_exists($locale->id,$response) ) {
+                    $response = $response+[$locale->id => $locale];
+                }
+            }
+            if( isset($locales_params['longitude']) &&
+                strpos(strtolower($locale->longitude),strtolower($locales_params['longitude'])) !==false ){
+                if ( !array_key_exists($locale->id,$response) ) {
+                    $response = $response+[$locale->id => $locale];
+                }
+            }
         }
-        return response()->json('Localidade nao encontrada', 404 );
+
+        if ( !count($response)) {
+            return response()->json('Localidade nao encontrada', 404 );
+        }
+         return response()->json($response);
     }
+
+
 }
